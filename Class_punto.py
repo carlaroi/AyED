64f6608288fe83dc238ebb873f2750bbf973077e
carlaroi <158266683+carlaroi@users.noreply.github.com>
2024-01-31 12:29:59 +0100

    Update Class_punto.py

--- Class_punto.py
+++ Class_punto.py
@@ -1,6 +1,10 @@
 import math
 
-class Punto:
-  def __init__(self, x=0, y=0)
+class Punto: #Creamos la clase punto 
+  def __init__(self, x=0, y=0) 
     self.x = x
     self.y = y
+
+  def __string__(self) #Sobreescribimos el método string para imprimir en el formato (x,y)
+    return f"({self.x, self.y})"
+    
